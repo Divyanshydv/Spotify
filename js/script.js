fe562eded91ec0c673bfe31c0e7420847f7fd55b
Divyanshydv <106545564+Divyanshydv@users.noreply.github.com>
2025-06-18 15:32:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log('Lets write JavaScript')
+console.log('Lets write JavaScript');
 let currentSong = new Audio();
 let songs;
 let currFolder;
@@ -19,40 +19,18 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-
-    // ✅ Now fetch JSON directly instead of folder listing
-    let res = await fetch(`/${folder}/info.json`);
-    let data = await res.json();
-    songs = data.songs;
-
-    // Show all the songs in the playlist
-    let songUL = document.querySelector(".songList ul");
-    songUL.innerHTML = "";
-
-    for (const song of songs) {
-        songUL.innerHTML += `
-            <li>
-                <img class="invert" width="34" src="img/music.svg" alt="">
-                <div class="info">
-                    <div>${song.replaceAll("%20", " ")}</div>
-                    <div>Harry</div>
-                </div>
-                <div class="playnow">
-                    <span>Play Now</span>
-                    <img class="invert" src="img/play.svg" alt="">
-                </div>
-            </li>`;
+    let a = await fetch(`/${folder}/`)
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(`/${folder}/`)[1])
+        }
     }
-
-    // Add event listeners
-    Array.from(songUL.getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click", () => {
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
-        });
-    });
-
-    
-}
  
 
 
@@ -83,7 +61,7 @@ async function getSongs(folder) {
 }
 
 const playMusic = (track, pause = false) => {
-    currentSong.src = `/${currFolder}/`+ track
+    currentSong.src = `/${currFolder}/` + track
     if (!pause) {
         currentSong.play()
         play.src = "img/pause.svg"
@@ -110,7 +88,7 @@ async function displayAlbums() {
             // Get the metadata of the folder
             let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json(); 
-            cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
+            cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
             <div class="play">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
