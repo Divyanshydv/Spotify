b9eb062a0d7f49fd7c8349c3a003d3767cc836d0
Divyanshydv <106545564+Divyanshydv@users.noreply.github.com>
2025-06-18 15:26:29 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,18 +19,40 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/${folder}`)
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/${folder}/`)[1])
-        }
+
+    // ✅ Now fetch JSON directly instead of folder listing
+    let res = await fetch(`/${folder}/info.json`);
+    let data = await res.json();
+    songs = data.songs;
+
+    // Show all the songs in the playlist
+    let songUL = document.querySelector(".songList ul");
+    songUL.innerHTML = "";
+
+    for (const song of songs) {
+        songUL.innerHTML += `
+            <li>
+                <img class="invert" width="34" src="img/music.svg" alt="">
+                <div class="info">
+                    <div>${song.replaceAll("%20", " ")}</div>
+                    <div>Harry</div>
+                </div>
+                <div class="playnow">
+                    <span>Play Now</span>
+                    <img class="invert" src="img/play.svg" alt="">
+                </div>
+            </li>`;
     }
+
+    // Add event listeners
+    Array.from(songUL.getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", () => {
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+        });
+    });
+
+    return songs;
+}
  
 
 
